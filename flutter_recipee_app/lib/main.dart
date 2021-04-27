bf5acef8b4bd0e5bfadc2b9faf9e0342e86adf24
Imran Sefat <39518388+ImranSefat@users.noreply.github.com>
2021-04-27 12:31:31 +0600

    Update main.dart

--- flutter_recipee_app/lib/main.dart
+++ flutter_recipee_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Recipe Guide',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
