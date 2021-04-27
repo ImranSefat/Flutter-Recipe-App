a3f5544a5b094b770894a607e44bff37ad4a84d8
Imran Sefat <39518388+ImranSefat@users.noreply.github.com>
2021-04-27 12:21:41 +0600

    Starter Template added

--- flutter_recipee_app/lib/Screens/HomePage.dart
+++ flutter_recipee_app/lib/Screens/HomePage.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
-import 'package:flutter_recipee_app/NewRecipe.dart';
-import 'package:tab_indicator_styler/tab_indicator_styler.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -12,94 +9,30 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        // color: Colors.grey[300],
-        height: 100,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(
-              FlutterIcons.home_outline_mco,
-              color: Colors.blue,
-            ),
-            Icon(
-              FlutterIcons.account_group_outline_mco,
-            ),
-            Icon(
-              FlutterIcons.heart_outlined_ent,
-            ),
-            Icon(
-              FlutterIcons.account_outline_mco,
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text("Recipe Guide"),
       ),
-      body: SafeArea(
-        bottom: false,
-        child: DefaultTabController(
-          length: 3,
-          initialIndex: 0,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Text(
+                "Don\'t forget to subscribe!",
+                style: Theme.of(context).textTheme.headline5,
+              ),
               SizedBox(
-                height: 40,
+                height: 10,
               ),
-              TabBar(
-                isScrollable: true,
-                indicatorColor: Colors.red,
-                tabs: [
-                  Tab(
-                    text: "New Recipes".toUpperCase(),
-                  ),
-                  Tab(
-                    text: "Favourites".toUpperCase(),
-                  ),
-                  Tab(
-                    text: "Categories".toUpperCase(),
-                  ),
-                ],
-                labelColor: Colors.black,
-                indicator: DotIndicator(
-                  color: Colors.black,
-                  distanceFromCenter: 16,
-                  radius: 3,
-                  paintingStyle: PaintingStyle.fill,
-                ),
-                unselectedLabelColor: Colors.black.withOpacity(0.3),
-                labelStyle: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w600,
-                ),
-                labelPadding: EdgeInsets.symmetric(
-                  horizontal: 24,
-                ),
+              Text(
+                "Most of you are not subscribed. It takes me days to make this kind of video.",
+                style: Theme.of(context).textTheme.headline6,
               ),
-              Expanded(
-                child: TabBarView(
-                  children: <Widget>[
-                    NewRecipe(),
-                    Container(
-                      child: Center(
-                        child: Text(
-                          'Favourite Section',
-                        ),
-                      ),
-                    ),
-                    Container(
-                      child: Center(
-                        child: Text(
-                          'Cetegories',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
             ],
           ),
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
